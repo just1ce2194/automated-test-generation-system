3f4e709b43050aa2778a3bb094bf9b878b78fbff
Vitaliy Zholobitskiy <vitaliy.wk@gmail.com>
2018-06-09 18:59:17 +0200

    inint

--- src/components/Questions/CheckBox.js
+++ src/components/Questions/CheckBox.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Radio from '@material-ui/core/Radio';
+import CheckBox from '@material-ui/core/Checkbox';
 
 const {Component} = React;
 
@@ -40,7 +40,7 @@ class CheckboxQuestion extends Component {
                 return <FormControlLabel
                     key={answer.answerId}
                     control={
-                        <Radio
+                        <CheckBox
                             checked={isChecked}
                             onChange={() => this.onAnswerChange( answer.answerId )}
                             color='primary'
