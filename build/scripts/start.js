ffddcb56835b90a79684be6a7bea3c4c251e2a44
Vitaliy Zholobitskiy <vitaliy.wk@gmail.com>
2018-06-09 19:20:07 +0200

    fixed start script error

--- build/scripts/start.js
+++ build/scripts/start.js
@@ -2,5 +2,5 @@ const logger = require('../lib/logger')
 
 logger.info('Starting server...')
 require('../../server/index').listen(process.env.PORT || 3000, () => {
-  logger.success('Server is running at)
+  logger.success('Server is running at')
 })
