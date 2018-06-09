0ffc8833cd297186bc4b60f18b5f38476d91c221
Vitaliy Zholobitskiy <vitaliy.wk@gmail.com>
2018-06-09 19:15:57 +0200

    added heroku port

--- build/scripts/start.js
+++ build/scripts/start.js
@@ -1,6 +1,6 @@
 const logger = require('../lib/logger')
 
 logger.info('Starting server...')
-require('../../server/index').listen(3000, () => {
-  logger.success('Server is running at http://localhost:3000')
+require('../../server/index').listen(process.env.PORT || 3000, () => {
+  logger.success('Server is running at)
 })
